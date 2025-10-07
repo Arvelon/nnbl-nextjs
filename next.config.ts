3556f53bc5c5d18180f60c310dd1130e64b88d85
Jonas Stasseyns <jonas.stasseyns@soleras.com>
2025-10-07 19:24:25 +0000

    s

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export',
+   images: {
+    unoptimized: true, // 👈 disables runtime optimization for static export
+  },
 };
 
 export default nextConfig;
